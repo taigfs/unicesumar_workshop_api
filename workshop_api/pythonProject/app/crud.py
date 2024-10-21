679fb55c14a0dd7eacd1538106afef09a5c3d927
Tainan Fidelis <taigfs@gmail.com>
2024-10-21 15:35:44 -0300

    refactor: refactor MongoDB connection string

--- workshop_api/pythonProject/app/crud.py
+++ workshop_api/pythonProject/app/crud.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from bson.objectid import ObjectId
 
 # Conexão com o banco de dados MongoDB
-client = AsyncIOMotorClient("mongodb://localhost:27017")
+client = AsyncIOMotorClient("mongodb://mongodb:27017")
 db = client["task_manager_db"]
 
 # Função para adicionar um novo usuário
